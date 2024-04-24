e2ee593e0d37cd391554dea2b085e75531974201
hylandtechoutreach <hylandtechoutreach@users.noreply.github.com>
2024-04-24 12:39:15 +0000

    Update gitbook 2024-04-24 12:39:15

--- passwordProtectedPages.js
+++ passwordProtectedPages.js
@@ -8,7 +8,7 @@ let passwordProtectedPages = {
     // "Week06/BananasFollowAlong.md": "potassium",
     // "Week07/PixelsCodeAlong.md": "arcade",
     // "Week08/MagazineWebsiteFollowAlongWithCode.md": "ohthehumanity",
-    "Week09/CrustaceansFollowAlong.md": "krabbypatty",
+    // "Week09/CrustaceansFollowAlong.md": "krabbypatty",
     "Week10/PresidentsFollowAlong.md": "commanderinchief",
     "Week10/AnimationFollowAlong.md": "life"
 }
